295cb4c9393ef77cd7ea110c12bcb2a6cec33ecd
JLCurtsinger <98992595+JLCurtsinger@users.noreply.github.com>
2025-08-10 15:03:52 -0700

    implement a robust fallback for /sitemap.xml

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+
+const SITE = 'https://gradeidea.cc'
+
+function xml(strings: TemplateStringsArray, ...vals: any[]) {
+  return strings.reduce((out, s, i) => out + s + (vals[i] ?? ''), '')
+}
+
+export async function GET() {
+  const today = new Date().toISOString().slice(0,10)
+  const body = xml`<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url><loc>${SITE}/</loc><lastmod>${today}</lastmod></url>
+    <url><loc>${SITE}/features</loc><lastmod>${today}</lastmod></url>
+    <url><loc>${SITE}/pricing</loc><lastmod>${today}</lastmod></url>
+    <url><loc>${SITE}/examples</loc><lastmod>${today}</lastmod></url>
+    <url><loc>${SITE}/founders-hub</loc><lastmod>${today}</lastmod></url>
+  </urlset>`
+
+  return new NextResponse(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=UTF-8',
+      'Cache-Control': 'public, s-maxage=1800, stale-while-revalidate=86400',
+    },
+  })
+}
+
+export const dynamic = 'force-static'
+export const revalidate = 3600
